5bc0c8ec1a64c2369dafecdcd9c9793ec46fffdf
Ashirvad <83695574+Ashirvad-Singh@users.noreply.github.com>
2022-07-05 18:02:12 +0530

    my first api app

---
+++ app.js
@@ -0,0 +1,32 @@
+const exp = require('constants');
+const { query } = require('express');
+const express = require('express');
+const app = express();
+const path = require('path');
+const request = require('request');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.get('/results', (req, res) => {
+    let query = req.query.search;
+    request('https://api.themoviedb.org/3/search/movie?api_key=3015837e19a8dc09f3a7186d872d977a&query=' + query, (error, response, body) => {
+        if (error) {
+            console.log(error);
+
+        }
+        let data = JSON.parse(body);
+        res.render('movies', {data:data,searchQuery:query
+        });
+    });
+
+
+
+});
+app.get('/search', (req, res) => {
+    res.render('search');
+
+});
+
+app.listen(3000, () => {
+    console.log('server started');
+});
